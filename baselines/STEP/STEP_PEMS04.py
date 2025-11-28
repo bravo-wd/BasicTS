a367998bad0db331dba20eb31f614696f234de40
bravo-wd <wdworkmail@qq.com>
2025-11-28 09:55:23 +0800

    step-pems04

--- baselines/STEP/STEP_PEMS04.py
+++ baselines/STEP/STEP_PEMS04.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import torch
 from easydict import EasyDict
 
 sys.path.append(os.path.abspath(__file__ + '/../../..'))
@@ -7,43 +8,73 @@ sys.path.append(os.path.abspath(__file__ + '/../../..'))
 from basicts.metrics import masked_mae, masked_mape, masked_rmse
 from basicts.scaler import ZScoreScaler
 from basicts.data import TimeSeriesForecastingDataset
+from basicts.runners import SimpleTimeSeriesForecastingRunner
 from basicts.utils import get_regular_settings
 
-from .arch import TSFormer
-from .runner import TSFormerRunner
+from .arch import STEP
+from .loss import step_loss
 
 ############################## Hot Parameters ##############################
 # Dataset & Metrics configuration
 DATA_NAME = 'PEMS04'  # Dataset name
 regular_settings = get_regular_settings(DATA_NAME)
-# INPUT_LEN = regular_settings['INPUT_LEN']  # Length of input sequence
-INPUT_LEN = 288 * 7
+INPUT_LEN_SHORT = regular_settings['INPUT_LEN']  # Length of input sequence
+INPUT_LEN = 288 * 7 * 2
 OUTPUT_LEN = regular_settings['OUTPUT_LEN']  # Length of output sequence
 TRAIN_VAL_TEST_RATIO = regular_settings['TRAIN_VAL_TEST_RATIO']  # Train/Validation/Test split ratios
 NORM_EACH_CHANNEL = regular_settings['NORM_EACH_CHANNEL']  # Whether to normalize each channel of the data
 RESCALE = regular_settings['RESCALE']  # Whether to rescale the data
 NULL_VAL = regular_settings['NULL_VAL']  # Null value in the data
 # Model architecture and parameters
-MODEL_ARCH = TSFormer
+MODEL_ARCH = STEP
 MODEL_PARAM = {
-    "patch_size": 12,
-    "in_channel": 1,
-    "embed_dim": 96,
-    "num_heads": 4,
-    "mlp_ratio": 4,
-    "dropout": 0.1,
-    "num_token": 288 * 7 / 12,
-    "mask_ratio": 0.75,
-    "encoder_depth": 4,
-    "decoder_depth": 1,
-    "mode": "pre-train"
+    "dataset_name": DATA_NAME,
+    "pre_trained_tsformer_path": "checkpoints/TSFormer/PEMS04_100_2016_12/3cef8458e288da6f11d8d9a28f789d90/TSFormer_best_val_MAE.pt",
+    "short_term_len": INPUT_LEN_SHORT,
+    "long_term_len": INPUT_LEN,
+    "tsformer_args": {
+        "patch_size": 12,
+        "in_channel": 1,
+        "embed_dim": 96,
+        "num_heads": 4,
+        "mlp_ratio": 4,
+        "dropout": 0.1,
+        "num_token": INPUT_LEN / 12,
+        "mask_ratio": 0.75,
+        "encoder_depth": 4,
+        "decoder_depth": 1,
+        "mode": "forecasting"
+    },
+    "backend_args": {
+        "num_nodes": 307,
+        "support_len": 2,
+        "dropout": 0.3,
+        "gcn_bool": True,
+        "addaptadj": True,
+        "aptinit": None,
+        "in_dim": 2,
+        "out_dim": 12,
+        "residual_channels": 32,
+        "dilation_channels": 32,
+        "skip_channels": 256,
+        "end_channels": 512,
+        "kernel_size": 2,
+        "blocks": 4,
+        "layers": 2
+    },
+    "dgl_args": {
+        "dataset_name": DATA_NAME,
+        "k": 10,
+        "input_seq_len": INPUT_LEN,
+        "output_seq_len": OUTPUT_LEN
+    }
 }
-NUM_EPOCHS = 150
+NUM_EPOCHS = 100
 
 ############################## General Configuration ##############################
 CFG = EasyDict()
 # General settings
-CFG.DESCRIPTION = 'Lab-TSFormer-PEMS04 Config'
+CFG.DESCRIPTION = 'An Example Config'
 # 优先从环境变量 BASICTS_GPU_NUM 里读 GPU 数量
 _env_gpu_num = os.environ.get("BASICTS_GPU_NUM")
 if _env_gpu_num is not None:
@@ -54,7 +85,7 @@ if _env_gpu_num is not None:
 else:
     CFG.GPU_NUM = 1  # 默认单卡
 # Runner
-CFG.RUNNER = TSFormerRunner
+CFG.RUNNER = SimpleTimeSeriesForecastingRunner
 
 ############################## Dataset Configuration ##############################
 CFG.DATASET = EasyDict()
@@ -86,8 +117,9 @@ CFG.MODEL = EasyDict()
 CFG.MODEL.NAME = MODEL_ARCH.__name__
 CFG.MODEL.ARCH = MODEL_ARCH
 CFG.MODEL.PARAM = MODEL_PARAM
-CFG.MODEL.FORWARD_FEATURES = [0]
+CFG.MODEL.FORWARD_FEATURES = [0, 1, 2]
 CFG.MODEL.TARGET_FEATURES = [0]
+CFG.MODEL.DDP_FIND_UNUSED_PARAMETERS = True
 
 ############################## Metrics Configuration ##############################
 
@@ -109,14 +141,14 @@ CFG.TRAIN.CKPT_SAVE_DIR = os.path.join(
     MODEL_ARCH.__name__,
     '_'.join([DATA_NAME, str(CFG.TRAIN.NUM_EPOCHS), str(INPUT_LEN), str(OUTPUT_LEN)])
 )
-CFG.TRAIN.LOSS = masked_mae
+CFG.TRAIN.LOSS = step_loss
 # Optimizer settings
 CFG.TRAIN.OPTIM = EasyDict()
 CFG.TRAIN.OPTIM.TYPE = "Adam"
 CFG.TRAIN.OPTIM.PARAM = {
     "lr": 0.002,
     "weight_decay": 1.0e-5,
-    "eps": 1.0e-8
+    "eps": 1.0e-8,
 }
 # Learning rate scheduler settings
 CFG.TRAIN.LR_SCHEDULER = EasyDict()
@@ -125,24 +157,24 @@ CFG.TRAIN.LR_SCHEDULER.PARAM = {
     "milestones": [1, 18, 36, 54, 72],
     "gamma": 0.5
 }
-# 提前停止
-CFG.TRAIN.EARLY_STOPPING_PATIENCE = None
 # Train data loader settings
 CFG.TRAIN.DATA = EasyDict()
 CFG.TRAIN.DATA.BATCH_SIZE = 8
 CFG.TRAIN.DATA.SHUFFLE = True
-CFG.TRAIN.DATA.NUM_WORKERS = min([os.cpu_count(), 8 if 8 > 1 else 0, 8])
+CFG.TRAIN.DATA.NUM_WORKERS = 2
 CFG.TRAIN.DATA.PIN_MEMORY = True
-CFG.TRAIN.CLIP_GRAD_PARAM = {
-    "max_norm": 3.0
-}
+# curriculum learning
+CFG.TRAIN.CL = EasyDict()
+CFG.TRAIN.CL.WARM_EPOCHS = 0
+CFG.TRAIN.CL.CL_EPOCHS = 6
+CFG.TRAIN.CL.PREDICTION_LENGTH = 12
 
 ############################## Validation Configuration ##############################
 CFG.VAL = EasyDict()
 CFG.VAL.INTERVAL = 1
 CFG.VAL.DATA = EasyDict()
 CFG.VAL.DATA.BATCH_SIZE = 8
-CFG.VAL.DATA.NUM_WORKERS = min([os.cpu_count(), 8 if 8 > 1 else 0, 8])
+CFG.VAL.DATA.NUM_WORKERS = 2
 CFG.VAL.DATA.PIN_MEMORY = True
 
 ############################## Test Configuration ##############################
@@ -150,7 +182,7 @@ CFG.TEST = EasyDict()
 CFG.TEST.INTERVAL = 1
 CFG.TEST.DATA = EasyDict()
 CFG.TEST.DATA.BATCH_SIZE = 8
-CFG.TEST.DATA.NUM_WORKERS = min([os.cpu_count(), 8 if 8 > 1 else 0, 8])
+CFG.TEST.DATA.NUM_WORKERS = 2
 CFG.TEST.DATA.PIN_MEMORY = True
 
 ############################## Evaluation Configuration ##############################
@@ -158,4 +190,5 @@ CFG.TEST.DATA.PIN_MEMORY = True
 CFG.EVAL = EasyDict()
 
 # Evaluation parameters
-CFG.EVAL.USE_GPU = True  # Whether to use GPU for evaluation. Default: True
+CFG.EVAL.HORIZONS = range(1, 13)  # 1, 2, ..., 12
+CFG.EVAL.USE_GPU = False  # Whether to use GPU for evaluation. Default: True
